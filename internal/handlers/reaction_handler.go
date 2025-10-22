10ce280f964b4008c7f8ecdd0f69b77375be5c39
jramoscl <joracle@protonmail.com>
2025-10-22 17:19:50 +0200

    finalisation 'reaction_handler' avec le referer

--- internal/handlers/reaction_handler.go
+++ internal/handlers/reaction_handler.go
@@ -9,30 +9,55 @@ import (
 /*MARK: React
  */
 func ReactHandler(w http.ResponseWriter, r *http.Request) {
-	userID := r.Context().Value("userID").(int64)
+	/*userID := r.Context().Value("userID").(int64)*/
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		http.Error(w, "❌ unauthenticated user", http.StatusUnauthorized)
+		return
+	}
+	// Vérifie l’utilisateur à partir du token
+	user, err := userService.Home(cookie.Value)
+	if err != nil || user == nil {
+		http.Error(w, "❌ invalid session", http.StatusUnauthorized)
+		return
+	}
+
 	targetType := r.URL.Query().Get("target_type")
 	targetID, _ := strconv.ParseInt(r.URL.Query().Get("target_id"), 10, 64)
 	value, _ := strconv.Atoi(r.URL.Query().Get("value"))
 
-	if err := reactionService.React(userID, targetType, targetID, value); err != nil {
+	if err := reactionService.React(user.ID, targetType, targetID, value); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "reaction saved: user=%d target=%s/%d value=%d\n", userID, targetType, targetID, value)
+	/*fmt.Fprintf(w, "reaction saved: user=%d target=%s/%d value=%d\n", user.ID, targetType, targetID, value)*/
+	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
 }
 
 /*MARK: RemoveReaction
  */
 func RemoveReactionHandler(w http.ResponseWriter, r *http.Request) {
-	userID := r.Context().Value("userID").(int64)
+	/*userID := r.Context().Value("userID").(int64)*/
+	cookie, err := r.Cookie("session_token")
+	if err != nil {
+		http.Error(w, "❌ unauthenticated user", http.StatusUnauthorized)
+		return
+	}
+	user, err := userService.Home(cookie.Value)
+	if err != nil || user == nil {
+		http.Error(w, "❌ invalid session", http.StatusUnauthorized)
+		return
+	}
+
 	targetType := r.URL.Query().Get("target_type")
 	targetID, _ := strconv.ParseInt(r.URL.Query().Get("target_id"), 10, 64)
 
-	if err := reactionService.RemoveReaction(userID, targetType, targetID); err != nil {
+	if err := reactionService.RemoveReaction(user.ID, targetType, targetID); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "reaction deleted: user=%d target=%s/%d\n", userID, targetType, targetID)
+	/*fmt.Fprintf(w, "reaction deleted: user=%d target=%s/%d\n", user.ID, targetType, targetID)*/
+	http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
 }
 
 /*MARK: GetCounts
